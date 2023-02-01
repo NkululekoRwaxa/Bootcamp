d6b5647b7798ff123da83b8e578519bdc9d63111
Nkululeko Rwaxa <46945487+NkululekoRwaxa@users.noreply.github.com>
2023-02-01 15:53:09 +0200

    Add files via upload

--- Chapter 14 If-Else/chapter14.js
+++ Chapter 14 If-Else/chapter14.js
@@ -5,13 +5,18 @@ function placeTheOrder() {
     var pizzas = document.getElementById("numPizzas").value;
     var kind = document.getElementById("typePizza").value;
     var place = document.getElementById("deliveryCity").value;
+    var birthday = document.getElementById("birthday").value;
 
     var orderPrice = calculatePrice(pizzas, kind);
-    var deliveryPrice = calculateDelivery(orderPrice, place);
+    var deliveryPrice = calculateDelivery(orderPrice, place, birthday);
 
     // create outut
     var theOutput = "<p>Thank you for your order.</p>";
-    theOutput += "<p>Your total is R:" + (orderPrice + deliveryPrice) + "</p>";
+    if (deliveryPrice === 0) {
+        theOutput += "<p>You get free delivery.</p>" + "<p>Your total is R:" + (orderPrice) + "</p>";
+    } else {
+        theOutput += "<p>Your delivery price is R: " + deliveryPrice + "</p>" + "<p>Your total is R:" + (orderPrice + deliveryPrice) + "</p>";
+    }
 
     // display output
     document.getElementById("displayTotal").innerHTML = theOutput;
@@ -22,14 +27,22 @@ function calculatePrice(pizzas, type) {
     var extraCharge = 0;
 
     // todo: calculate extraCharge, if there is one.
-
+    if (type === "supreme") {
+        extraCharge = Number(pizzas) * 2;
+        orderPrice += extraCharge;
+    }
     return orderPrice;
 }
 
-function calculateDelivery(orderPrice, deliveryCity) {
+function calculateDelivery(orderPrice, deliveryCity, birthDay) {
     var deliveryPrice = 0;
 
     // todo: calculate delivery price, if there is one
+    if (((deliveryCity === "anytown") && (orderPrice > 10)) || (birthday === "yes")) {
+        deliveryPrice = 0;
+    } else {
+        deliveryPrice = 5;
+    }
 
     return deliveryPrice;
 }
