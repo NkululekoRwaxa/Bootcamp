0d8f25f116680892e69b45ba0e660dd12edd1e1a
Nkululeko Rwaxa <46945487+NkululekoRwaxa@users.noreply.github.com>
2023-02-07 10:59:05 +0200

    Add files via upload

--- Chapter 5 JavaScript & HTML/chapter5.js
+++ Chapter 5 JavaScript & HTML/chapter5.js
@@ -1,20 +1,23 @@
 // change body background color
 document.body.style.backgroundColor = "lightblue";
 
-document.getElementById("myName").innerHTML = "Nkululeko";
-
 // center align text 
 document.getElementById("myName").style.textAlign = "center";
 document.getElementById("aboutMe").style.textAlign = "center";
+document.getElementById("buttonP").style.textAlign = "center";
 
 document.getElementById("changeList").style.width = "300px";
 document.getElementById("changeList").style.height = "25px";
+
 // change list on button click
 document.getElementById("changeList").onclick = newList;
+// change heading on button click
+document.getElementById("changeHeading").onclick = newHeading;
 
 var item1;
 var item2;
 var item3;
+var heading;
 
 function newList() {
     item1 = prompt("Enter a new first thing: ");
@@ -23,14 +26,25 @@ function newList() {
     updateList();
 }
 
+function newHeading() {
+    heading = prompt("Enter a new heading: ");
+    updateHeading();
+}
+
+function updateHeading() {
+    if (heading === "" || heading === null) {
+        return;
+    } else {
+        document.getElementById("myName").innerHTML = heading;
+    }
+}
+
 function updateList() {
-    if (item1 !== null) {
+    if (item1 === null || item2 === null || item3 === null || item1 === "" || item2 === "" || item3 === "") {
+        return;
+    } else {
         document.getElementById("firstThing").innerHTML = item1;
-    }
-    if (item2 !== null) {
         document.getElementById("secondThing").innerHTML = item2;
-    }
-    if (item3 !== null) {
         document.getElementById("thirdThing").innerHTML = item3;
     }
 }
