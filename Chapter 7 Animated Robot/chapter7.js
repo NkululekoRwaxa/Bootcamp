b8393d3266f3850b1a2d420f63fff53a40f28b38
Nkululeko Rwaxa <46945487+NkululekoRwaxa@users.noreply.github.com>
2023-02-07 11:00:37 +0200

    Add files via upload

--- Chapter 7 Animated Robot/chapter7.js
+++ Chapter 7 Animated Robot/chapter7.js
@@ -1,12 +1,10 @@
 var rightEye = document.getElementById("righteye");
 var leftEye = document.getElementById("lefteye");
 var leftArm = document.getElementById("leftarm");
-var head = document.getElementById("head");
 
 rightEye.addEventListener("click", moveUpDown);
 leftEye.addEventListener("click", moveUpDown);
 leftArm.addEventListener("click", moveRightLeft);
-head.addEventListener("click", moveUpDownHead);
 
 function moveUpDown(e) {
     var robotPart = e.target;
@@ -24,21 +22,6 @@ function moveUpDown(e) {
 
 }
 
-function moveUpDownHead(e) {
-    var robotPart = e.target;
-    var top = 0;
-
-    var id = setInterval(frame, 10) // draw every 10ms
-
-    function frame() {
-        robotPart.style.top = top + '%';
-        top++;
-        if (top === 6) {
-            clearInterval(id);
-        }
-    }
-}
-
 function moveRightLeft(e) {
     var robotPart = e.target;
     var left = 0;
@@ -51,3 +34,29 @@ function moveRightLeft(e) {
         }
     }
 }
+
+/* animate text using DOM */
+var leftOffset = 0;
+var rightOffset = 1050;
+
+var moveText1 = function() {
+    $("#text1").offset({ left: leftOffset });
+
+    leftOffset++;
+
+    if (leftOffset > 1050) {
+        leftOffset = 0;
+    }
+};
+
+var moveText2 = function() {
+    $("#text2").offset({ left: rightOffset });
+
+    rightOffset--;
+
+    if (rightOffset < 0) {
+        rightOffset = 1050;
+    }
+};
+setInterval(moveText1, 10);
+setInterval(moveText2, 10);
