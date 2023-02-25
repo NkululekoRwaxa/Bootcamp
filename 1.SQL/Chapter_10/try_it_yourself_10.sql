f1df585c53bbd506825173245ccb78cf2505a965
Nkululeko Rwaxa <46945487+NkululekoRwaxa@users.noreply.github.com>
2023-02-25 19:51:38 +0200

    Add files via upload

---
+++ 1.SQL/Chapter_10/try_it_yourself_10.sql
@@ -0,0 +1,50 @@
+-- CORRECTIONS
+-- 1.
+SELECT
+    round(
+      corr(median_hh_income, pct_bachelors_higher)::numeric, 2
+      ) AS bachelors_income_r,
+    round(
+      corr(median_hh_income, pct_masters_higher)::numeric, 2
+      ) AS masters_income_r
+FROM acs_2011_2015_stats;
+
+-- 2.
+SELECT
+    city,
+    st,
+    population,
+    motor_vehicle_theft,
+    round(
+        (motor_vehicle_theft::numeric / population) * 100000, 1
+        ) AS vehicle_theft_per_100000
+FROM fbi_crime_data_2015
+WHERE population >= 500000
+ORDER BY vehicle_theft_per_100000 DESC;
+
+-- b) In 2015, Detroit and Memphis had the two highest rates of violent crime.
+
+SELECT
+    city,
+    st,
+    population,
+    violent_crime,
+    round(
+        (violent_crime::numeric / population) * 100000, 1
+        ) AS violent_crime_per_100000
+FROM fbi_crime_data_2015
+WHERE population >= 500000
+ORDER BY violent_crime_per_100000 DESC;
+
+-- 3.
+SELECT
+    libname,
+    stabr,
+    visits,
+    popu_lsa,
+    round(
+        (visits::numeric / popu_lsa) * 1000, 1
+        ) AS visits_per_1000,
+    rank() OVER (ORDER BY (visits::numeric / popu_lsa) * 1000 DESC)
+FROM pls_fy2014_pupld14a
+WHERE popu_lsa >= 250000;
